c99976afa4d850a8e045a5165f706669ef819f96
Susobhan Dash <susobhan.dash@gmail.com>
2023-10-11 00:19:33 +0530

    Initial Push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,121 @@
 import { Component } from '@angular/core';
+import { ListboxChangeEvent } from 'primeng/listbox';
+
+export interface ThemeOption {
+  text: string;
+  value: ThemeOptions;
+}
+
+export enum ThemeOptions {
+  Red = 'red',
+  Aqua = 'aqua',
+  Blue = 'blue',
+  Green = 'green',
+  DarkGreen = 'dark-green',
+  Teal = 'teal',
+  Purple = 'purple',
+  Orange = 'orange',
+  Violet = 'violet',
+  PinkRed = 'pink-red',
+  YellowBrown = 'yellow-brown',
+  DarkBlue = 'dark-blue',
+  Brown = 'brown',
+}
+
+export enum ThemeModes {
+  Light = 'light-theme',
+  Dark = 'dark-theme',
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'custom-themes';
+  title = 'Custom Themes';
+  themeOptions: ThemeOption[] = [
+    {
+      text: 'Red',
+      value: ThemeOptions.Red,
+    },
+    {
+      text: 'Blue',
+      value: ThemeOptions.Blue,
+    },
+    {
+      text: 'Dark Blue',
+      value: ThemeOptions.DarkBlue,
+    },
+    {
+      text: 'Aqua',
+      value: ThemeOptions.Aqua,
+    },
+    {
+      text: 'Green',
+      value: ThemeOptions.Green,
+    },
+    {
+      text: 'Dark Green',
+      value: ThemeOptions.DarkGreen,
+    },
+    {
+      text: 'Teal',
+      value: ThemeOptions.Teal,
+    },
+    {
+      text: 'Purple',
+      value: ThemeOptions.Purple,
+    },
+    {
+      text: 'Orange',
+      value: ThemeOptions.Orange,
+    },
+    {
+      text: 'Violet',
+      value: ThemeOptions.Violet,
+    },
+    {
+      text: 'Pink Red',
+      value: ThemeOptions.PinkRed,
+    },
+    {
+      text: 'Yellow Brown',
+      value: ThemeOptions.YellowBrown,
+    },
+    {
+      text: 'Gold Brown',
+      value: ThemeOptions.Brown,
+    },
+  ];
+
+  activeTheme: ThemeOption = this.themeOptions[0];
+  themeMode: ThemeModes = ThemeModes.Dark;
+  themeModes = ThemeModes;
+
+  constructor() {
+    this.themeChanged();
+    this.themeModeToggled();
+  }
+
+  themeChanged() {
+    console.log(this.activeTheme);
+
+    this.themeOptions.forEach((theme: ThemeOption) => {
+      document.querySelector('body')?.classList.remove(theme.value);
+    });
+
+    document.querySelector('body')?.classList.add(this.activeTheme.value);
+  }
+
+  themeModeToggled() {
+    document.querySelector('body')?.classList.remove(this.themeMode);
+    if (this.themeMode === ThemeModes.Light) {
+      this.themeMode = ThemeModes.Dark;
+    } else {
+      this.themeMode = ThemeModes.Light;
+    }
+
+    document.querySelector('body')?.classList.add(this.themeMode);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { ListboxModule } from 'primeng/listbox';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    OverlayPanelModule,
+    ToolbarModule,
+    ButtonModule,
+    ListboxModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
